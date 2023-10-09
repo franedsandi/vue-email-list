7b15e21be2245dd91b8dbc19832bdc51ef561dd1
franedsandi <franedsandi@gmail.com>
2023-10-09 10:18:32 +0200

    correzioni in lezione

--- js/script.js
+++ js/script.js
@@ -3,28 +3,27 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      titolo:'Elenco email',
       apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail",
       emails: [],
-    };
-  },
-  mounted() {
-    this.getRandomEmails();
+      limit: 10,
+    }
   },
   methods: {
-    getRandomEmails() {
-      const emails = [];
-      const getEmail = () => {
-        axios.get(this.apiUrl)
-          .then((response) => {
-            emails.push(response.data.response);
-            if (emails.length < 10) {
-              getEmail();
-            } else {
-              this.emails = emails;
-            }
-          });
-      };
-      getEmail();
-    } 
-  },
+    getApi() {
+      axios.get(this.apiUrl)
+        .then((result) => {
+        this.emails.push(result.data.response);
+        console.log(this.emails)
+      })      
+    },
+    startGetEmails(){
+      for(let i = 1; i <= this.limit; i++){
+      this.getApi()
+      }
+    }
+  }, 
+  mounted(){
+    this.startGetEmails();
+  } 
 }).mount("#app");
